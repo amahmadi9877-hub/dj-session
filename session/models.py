094b082a2157c9cd5c9d5f85ef448332344b84fb
amahmadi9877-hub <am.ahmadi9877@gmail.com>
2026-07-06 14:03:42 +0330

    Create session model

--- session/models.py
+++ session/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Session(models.Mode):
+    session_id = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    expire_date = models.DateTimeField()
